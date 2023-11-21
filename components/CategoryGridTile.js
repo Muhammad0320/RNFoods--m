10d8b1c1eca71a5faaa40b1e3b3d1a61b9ad1448
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-21 04:21:57 +0100

    chore: styled category grid item

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,4 +1,4 @@
-import { Pressable, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 
 function CategoryGridTile({ title, color }) {
   return (
@@ -11,5 +11,37 @@ function CategoryGridTile({ title, color }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  outerContainer: {
+    padding: 16,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    textShadowRadius: 8,
+    shadowOffset: { width: 0.5, height: 2 },
+  },
+
+  innerContainer: {
+    borderRadius: 8,
+    justifyContent: "center",
+    alignContent: "center",
+    padding: 16,
+  },
+
+  button: {
+    opacity: 0.75,
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
 
 export default CategoryGridTile;
