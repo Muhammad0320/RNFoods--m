0e1d553ccc2ba41195921ef32aa066a4150beffd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-21 04:47:48 +0100

    prepare config for react navigation

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar, StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <>
       {" "}
-      <StatusBar styles="light" />
+      <StatusBar styles="dark" />
+      <NavigationContainer></NavigationContainer>
       <CategoriesScreen />{" "}
     </>
   );
